43d308551e7e8647784c6c9e166b27213b59d84e
James Reilly <jreilly1821@gmail.com>
2025-07-31 23:16:29 +0530

    Create build-iso.sh
    Signed-off-by: James Reilly <jreilly1821@gmail.com>

---
+++ build-iso.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <image_uri>"
+  echo "Example: $0 ghcr.io/tuna-os/yellowfin:latest"
+  exit 1
+fi
+
+IMAGE_URI="$1"
+TOML_FILE="iso.toml"
+
+# Create the TOML file with dynamic content
+cat << EOF > "$TOML_FILE"
+[customizations.installer.kickstart]
+contents = """
+%post
+bootc switch --mutate-in-place --transport registry --enforce-container-sigpolicy $IMAGE_URI
+%end
+"""
+
+[customizations.installer.modules]
+enable = [
+  "org.fedoraproject.Anaconda.Modules.Storage"
+]
+disable = [
+  "org.fedoraproject.Anaconda.Modules.Network",
+  "org.fedoraproject.Anaconda.Modules.Security",
+  "org.fedoraproject.Anaconda.Modules.Services",
+  "org.fedoraproject.Anaconda.Modules.Users",
+  "org.fedoraproject.Anaconda.Modules.Subscription",
+  "org.fedoraproject.Anaconda.Modules.Timezone"
+]
+EOF
+
+echo "Generated $TOML_FILE with content:"
+cat "$TOML_FILE"
+echo ""
+
+# Pull the image
+echo "Pulling image: $IMAGE_URI"
+sudo podman pull "$IMAGE_URI"
+
+# Run the bootc-image-builder command
+echo "Running bootc-image-builder..."
+sudo podman run --rm -it --privileged \
+  -v "$(pwd)":/output \
+  -v "$(pwd)/$TOML_FILE":/config.toml \
+  quay.io/centos-bootc/bootc-image-builder:latest \
+  build --type iso \
+  "$IMAGE_URI"
+
+echo "Script finished."
