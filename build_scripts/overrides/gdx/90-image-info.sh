c33452a81a575f547d196b381234124a34bdc297
James Reilly <jreilly1821@gmail.com>
2025-09-04 13:32:31 +0530

    Remove IMAGE_NAME and IMAGE_REF exports from 90-image-info.sh to simplify script environment

--- build_scripts/overrides/gdx/90-image-info.sh
+++ build_scripts/overrides/gdx/90-image-info.sh
@@ -4,8 +4,5 @@ set -euox pipefail
 source /run/context/build_scripts/lib.sh
 
 FLAVOR="gdx"
-IMAGE_REF="ostree-image-signed:docker://ghcr.io/ublue-os/${IMAGE_NAME}"
 export FLAVOR
-export IMAGE_NAME
-export IMAGE_REF
 "/run/context/build_scripts/scripts/image-info-set"
