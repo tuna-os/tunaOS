be73d90b56773edee492a4c21f946ebe187b313d
James Reilly <jreilly1821@gmail.com>
2026-03-10 01:52:38 +0530

    fix(build): only modify systemd-resolved if it exists
    This fixes builds where the systemd-resolved service is not installed by avoiding attempting to modify a missing file.

--- build_scripts/40-services.sh
+++ build_scripts/40-services.sh
@@ -58,9 +58,11 @@ authselect enable-feature with-silent-lastlog
 # Enable polkit rules for fingerprint sensors via fprintd
 authselect enable-feature with-fingerprint
 
-sed -i -e "s@PrivateTmp=.*@PrivateTmp=no@g" /usr/lib/systemd/system/systemd-resolved.service
-# FIXME: this does not yet work, the resolution service fails for somer reason
-# enable systemd-resolved for proper name resolution
-systemctl enable systemd-resolved.service
+if [[ -f /usr/lib/systemd/system/systemd-resolved.service ]]; then
+    sed -i -e "s@PrivateTmp=.*@PrivateTmp=no@g" /usr/lib/systemd/system/systemd-resolved.service
+    # FIXME: this does not yet work, the resolution service fails for somer reason
+    # enable systemd-resolved for proper name resolution
+    systemctl enable systemd-resolved.service
+fi
 
 printf "::endgroup::\n"
