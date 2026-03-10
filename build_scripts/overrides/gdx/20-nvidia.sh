341d136f3308ca0286d0935af0a12920e1c6ff54
James Reilly <jreilly1821@gmail.com>
2026-03-10 06:01:32 +0530

    fix(build): mask kernel-install dracut scriptlet for fedora gdx
    During the installation of the new kernel alongside NVIDIA drivers, the rpm-ostree kernel-install scriptlet tries to run dracut and fails due to the container build environment. Temporarily masking it allows the transaction to succeed.

--- build_scripts/overrides/gdx/20-nvidia.sh
+++ build_scripts/overrides/gdx/20-nvidia.sh
@@ -19,7 +19,10 @@ else
     dnf versionlock delete kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-uki-virt || true
     rpms=$(find /tmp/akmods-nvidia-open-rpms/ /tmp/kernel-rpms/ -name "*.rpm" -type f | tr '\n' ' ')
     if [ -n "$rpms" ]; then
+        # Mask rpm-ostree kernel-install script to prevent dracut errors during container build
+        mv /usr/lib/kernel/install.d/05-rpmostree.install /tmp/05-rpmostree.install.bak || true
         dnf install -y $rpms
+        mv /tmp/05-rpmostree.install.bak /usr/lib/kernel/install.d/05-rpmostree.install || true
     fi
     dnf versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-uki-virt
 fi
