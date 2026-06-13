a23f669a9d58f167b91a96249674fb62e68dcb1a
James Reilly <jreilly1821@gmail.com>
2026-06-13 17:50:56 +0100

    fix: add grouper to get-base-image.sh variant map

--- scripts/get-base-image.sh
+++ scripts/get-base-image.sh
@@ -9,6 +9,7 @@ case "$variant" in
 "albacore" | "almalinux") echo "quay.io/almalinuxorg/almalinux-bootc:10" ;;
 "skipjack" | "centos" | "lts") echo "quay.io/centos-bootc/centos-bootc:stream10" ;;
 "bonito" | "fedora" | "bluefin") echo "quay.io/fedora/fedora-bootc:43" ;;
+  "grouper" | "ubuntu") echo "ghcr.io/tuna-os/ubuntu:latest" ;;
 "bonito-rawhide" | "rawhide") echo "quay.io/fedora/fedora-bootc:rawhide" ;;
 "redfin" | "rhel") echo "registry.redhat.io/rhel10/rhel-bootc:latest" ;;
 *)
