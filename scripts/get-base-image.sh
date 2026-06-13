31eed91cb650a36312b8d343c7dcdb95ee433dfd
James Reilly <jreilly1821@gmail.com>
2026-06-13 17:56:26 +0100

    fix: switch ubuntu base image to ghcr.io/hanthor/ubuntu-26.04-desktop-bootc
    ghcr.io/tuna-os/ubuntu returns 403 Forbidden. Use the known-working
    hanthor image as referenced in the RFC.

--- scripts/get-base-image.sh
+++ scripts/get-base-image.sh
@@ -9,7 +9,7 @@ case "$variant" in
 "albacore" | "almalinux") echo "quay.io/almalinuxorg/almalinux-bootc:10" ;;
 "skipjack" | "centos" | "lts") echo "quay.io/centos-bootc/centos-bootc:stream10" ;;
 "bonito" | "fedora" | "bluefin") echo "quay.io/fedora/fedora-bootc:43" ;;
-  "grouper" | "ubuntu") echo "ghcr.io/tuna-os/ubuntu:latest" ;;
+  "grouper" | "ubuntu") echo "ghcr.io/hanthor/ubuntu-26.04-desktop-bootc:latest" ;;
 "bonito-rawhide" | "rawhide") echo "quay.io/fedora/fedora-bootc:rawhide" ;;
 "redfin" | "rhel") echo "registry.redhat.io/rhel10/rhel-bootc:latest" ;;
 *)
